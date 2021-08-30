d212a03e2de5f732d130e2d9d689cb4fd8459088
Dany Sam Salmon <danisam17@gmail.com>
2021-08-30 11:12:55 +0530

    Add print to show bitbar data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"math/rand"
@@ -12,8 +13,13 @@ import (
 )
 
 func main() {
-	c := make(chan int64)
-	// ctx := context.Background()
+	c := make(chan []int64)
+
+	latencies := []int64{9999999, 9999999}
+	ctx := context.Background()
+
+	ctxWithTimeout, cancelCtx := context.WithTimeout(ctx, 1*time.Second)
+	defer cancelCtx()
 
 	conn, err := icmp.ListenPacket("udp4", "0.0.0.0")
 	if err != nil {
@@ -21,18 +27,49 @@ func main() {
 	}
 	defer conn.Close()
 
-	go ping(conn, "1.1.1.1", 5233, c)
-	go ping(conn, "8.8.8.8", 5894, c)
+	go pingWithTimeout(ctx, conn, c)
 
-	t1 := <-c
-	t2 := <-c
+	select {
+	case <-ctxWithTimeout.Done():
+		latencies = []int64{10000, 10000}
+	case latencies = <-c:
+	}
 
-	fmt.Println("t1", t1)
-	fmt.Println("t2", t2)
+	latency := int64(0)
+	for _, l := range latencies {
+		latency = latency + l
+	}
+	print("", latency/int64(len(latencies)), false)
+	print("Google: ", latencies[0], true)
+	print("Cloudflare: ", latencies[1], true)
+}
+
+func print(text string, ping int64, dropdown bool) {
+	color := "green"
+	switch {
+	case ping < 100:
+		color = "green"
+	case ping < 500:
+		color = "orange"
+	case ping < 1000:
+		color = "purple"
+	default:
+		color = "red"
+	}
+	fmt.Printf("%s%dms|color=\"%s\" dropdown=\"%t\" font=\"Hack\"\n", text, ping, color, dropdown)
+}
+
+func pingWithTimeout(_ context.Context, conn net.PacketConn, c chan []int64) {
+	ch1 := make(chan int64)
+	ch2 := make(chan int64)
+	go ping(conn, "8.8.8.8", 5894, ch1)
+	go ping(conn, "1.1.1.1", 5233, ch2)
+
+	t1, t2 := <-ch1, <-ch2
+	c <- []int64{t1, t2}
 }
 
 func ping(conn net.PacketConn, ip string, seq int, c chan int64) {
-
 	pingInMs := int64(9999999)
 	defer func() {
 		c <- pingInMs
